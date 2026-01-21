da69cdafb88f415d5ef0ca9d7688905d1cf1e79c
ougajs-sys <ougajs@gmail.com>
2026-01-21 19:03:42 +0000

    Add BugSnag configuration file

--- src/lib/bugsnag.ts
+++ src/lib/bugsnag.ts
@@ -1,17 +1,7 @@
 import Bugsnag from '@bugsnag/js';
 
-Bugsnag.start({
-    apiKey: 'YOUR_API_KEY',
-    appVersion: '1.0.0',
-    releaseStage: 'production',
-    notifyReleaseStages: ['production', 'staging'],
-    enabledReleaseStages: ['production', 'staging'],
-    onError: (event) => {
-        // Customize the error event before it is sent to Bugsnag
-        event.addMetadata('user', {
-            // Custom user metadata
-        });
-    }
+const bugsnagClient = Bugsnag.createClient({
+  apiKey: process.env.VITE_BUGSNAG_API_KEY,
 });
 
-export default Bugsnag;
+export default bugsnagClient;
